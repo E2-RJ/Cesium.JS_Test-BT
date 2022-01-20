375c22a9b1f9f5f4c6f768410e0d78d9510ccc3c
E2-RJ <96015510+E2-RJ@users.noreply.github.com>
2022-01-20 13:38:23 +0000

    Update app.js
    Added render on request functionality, potentially reducing resource usage

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-// Your access token can be found at: https://cesium.com/ion/tokens.
-// Replace `your_access_token` with your Cesium ion access token.
+// Access token, found at: https://cesium.com/ion/tokens.
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNzMyNjg5Zi00ZTI5LTRmYWEtOTE3Mi1kOTk3Y2RjYjY1YWEiLCJpZCI6NzQ1MjEsImlhdCI6MTYzNzgyMzU4N30.JFkVpFHpCeYuhC4KDr5_Or8PZArO_v8ed9nuP2j1xUU';
 
 const viewer = new Cesium.Viewer('cesiumContainer', {
-  terrainProvider: Cesium.createWorldTerrain()
+    requestRenderMode: true, // Only render on request from entity, reduces CPU usage
+    maximumRenderTimeChange: Infinity,
+    terrainProvider: Cesium.createWorldTerrain()
 });
 
-// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings()); // Add Cesium OSM Buildings, a global 3D buildings layer.
 
-const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
+var scene = viewer.scene;
+scene.debugShowFramesPerSecond = false; // Shows FPS counter overlay, mainly for debugging purposes
+
+// Clear scene and set default view.
+var handler;
+function resetScene() {
+    viewer.trackedEntity = undefined;
+    viewer.dataSources.removeAll();
+    viewer.entities.removeAll();
+    viewer.scene.primitives.remove(tileset);
+    viewer.clock.shouldAnimate = false;
+    handler = handler && handler.destroy();
+    scene.skyBox.show = true;
+    scene.camera.flyHome(0.0);
+    scene.requestRender();
+    viewModel.showTimeOptions = false;
+    viewModel.timeChangeEnabled = false;
+    viewModel.maximumRenderTimeChange = 0;
+}
